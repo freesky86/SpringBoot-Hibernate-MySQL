d0c23cbd7293cfbe9835ae2c17ba6eb60ba34709
freesky86 <freesky_zh@hotmail.com>
2021-06-21 09:37:07 +0800

    Create SpringBootHibernateMySqlApplication.java
    create from http://start.spring.io

---
+++ src/main/java/com/example/SpringBootHibernateMySQL/SpringBootHibernateMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootHibernateMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateMySqlApplication.class, args);
+	}
+
+}
