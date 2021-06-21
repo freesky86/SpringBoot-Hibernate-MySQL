ae225c8a6a8e884cb159025e44eae4f118760a33
freesky <freesky_zh@hotmail.com>
2021-06-21 18:18:17 +0800

    Insert data when start

--- src/main/java/com/javadevjournal/SpringBootHibernateApplication.java
+++ src/main/java/com/javadevjournal/SpringBootHibernateApplication.java
@@ -1,7 +1,5 @@
 package com.javadevjournal;
 
-import com.javadevjournal.data.Customer;
-import com.javadevjournal.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -10,30 +8,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.javadevjournal.data.Customer;
+import com.javadevjournal.repository.CustomerRepository;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringBootHibernateApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(SpringBootHibernateApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(SpringBootHibernateApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHibernateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateApplication.class, args);
+    }
 
-	/*@Bean
-	public CommandLineRunner demo(CustomerRepository repository) {
-		return (args) -> {
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
 
-			repository.save(new Customer("Umesh", "Awasthi","email@test.com"));
-			repository.save(new Customer("David", "Dobrik", "email1@test.com"));
-			repository.save(new Customer("Robert", "Hickle","r.k@email.com"));
-			repository.save(new Customer("Edgar", "Smith","edgar@email.com"));
+            repository.save(new Customer("Umesh", "Awasthi", "email@test.com"));
+            repository.save(new Customer("David", "Dobrik", "email1@test.com"));
+            repository.save(new Customer("Robert", "Hickle", "r.k@email.com"));
+            repository.save(new Customer("Edgar", "Smith", "edgar@email.com"));
 
-			// fetch all customers
+            // fetch all customers
 
-			for (Customer customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-		};
-	}*/
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+        };
+    }
 }
